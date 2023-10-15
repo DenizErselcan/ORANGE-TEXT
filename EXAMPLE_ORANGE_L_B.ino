c3150e4b5d10c2c44e5a1ca8560d1e4a82eb1fc9
DenizErselcan <141042171+DenizErselcan@users.noreply.github.com>
2023-10-15 15:28:40 +0300

    Add files via upload
    Example code

---
+++ EXAMPLE_ORANGE_L_B.ino
@@ -0,0 +1,10 @@
+
+#include <ORANGELib.h>
+
+ORANGELib orange(0x27,24,4); // Create an instance of ORANGELib
+
+void setup() {
+   orange.displayORANGE(); // Display "ORANGE" on the LCD
+}
+
+void loop() { }
